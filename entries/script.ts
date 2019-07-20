84fa83a98aed79ded0b80c5585af395f2c9d7066
whatasoda <a16dc107@dhw.ac.jp>
2019-07-20 15:05:42 +0900

    rename pakages to src

--- entries/script.ts
+++ entries/script.ts
@@ -2,6 +2,6 @@ import './index.html';
 import React from 'react';
 import { render } from 'react-dom';
 // import Nyoro from '../packages/nyoro/components/Nyoro';
-import Byon from '../packages/nyoro/components/Byon';
+import Byon from '../src/nyoro/components/Byon';
 
 render(React.createElement(Byon, {}), document.getElementById('App'));
