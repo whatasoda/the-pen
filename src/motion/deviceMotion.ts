4759cd1cfa2f5bf9b110793ec3a75a1e148c87b4
whatasoda <git@whatasoda.me>
2019-09-07 14:52:45 +0900

    chore: minor change

--- src/motion/deviceMotion.ts
+++ src/motion/deviceMotion.ts
@@ -1,4 +1,4 @@
-import { quat, vec3, mat4 } from 'gl-matrix';
+import { quat, vec3 } from 'gl-matrix';
 import { eulerToArray, cartesianToArray } from '../utils/converter';
 import { completeAxis } from '../utils/vector';
 import createStabilizer from './stabilizer';
@@ -26,11 +26,11 @@ const createDeviceMotion = ({ direction, elasticity, viscous }: DeviceMotionCons
   const C_2 = vec3.create(); // y
   completeAxis(C_0, C_1, C_2, direction);
 
-  const P_0_curr = vec3.create();
-  const P_0_prev = vec3.create();
+  // const P_0_curr = vec3.create();
+  // const P_0_prev = vec3.create();
 
-  const P_1_curr = vec3.create();
-  const P_1_prev = vec3.create();
+  // const P_1_curr = vec3.create();
+  // const P_1_prev = vec3.create();
 
   const P_2_curr = vec3.create();
   const P_2_prev = vec3.create();
@@ -38,9 +38,14 @@ const createDeviceMotion = ({ direction, elasticity, viscous }: DeviceMotionCons
   let twist: number = 0;
   let first: boolean = true;
 
-  const m = mat4.identity(mat4.create());
-  mat4.multiplyScalar(m, m, 0.01);
   const attack = vec3.create();
+
+  const madgwick = new AHRS({
+    algorithm: 'Madgwick',
+    sampleInterval: 60,
+  });
+  const G = vec3.create();
+  const D2R = Math.PI / 180;
 
   const deviceMotion = (input: DeviceMotionInput, cb: (k: string, v: number) => void = () => {}) => {
     eulerToArray(input.orientation).forEach((v, i) => cb(`orientation:${i}`, v));
@@ -51,19 +56,12 @@ const createDeviceMotion = ({ direction, elasticity, viscous }: DeviceMotionCons
 
     const dt = input.interval;
     updateHeadMovement(dt);
-    end();
 
     const movment = [...V_movement];
     const { acceleration, velocity, jerk } = stabilizer(movment as V3, dt);
 
     return { acceleration, velocity, jerk, twist, movment, attack: [...attack] };
   };
-  const madgwick = new AHRS({
-    algorithm: 'Madgwick',
-    sampleInterval: 60,
-  });
-  const a = vec3.create();
-  const D2R = Math.PI / 180;
 
   const applyInput = ({
     interval: dt,
@@ -72,22 +70,17 @@ const createDeviceMotion = ({ direction, elasticity, viscous }: DeviceMotionCons
     orientation,
     rotationRate,
   }: DeviceMotionInput) => {
-    vec3.sub(a, cartesianToArray(accelerationIncludingGravity), cartesianToArray(acceleration));
+    vec3.copy(G, cartesianToArray(accelerationIncludingGravity));
+    // vec3.sub(G, cartesianToArray(accelerationIncludingGravity), cartesianToArray(acceleration));
     const [gx, gy, gz] = eulerToArray(rotationRate);
     const [mx, my, mz] = eulerToArray(orientation);
-    madgwick.update(gx * D2R, gy * D2R, gz * D2R, a[0], a[1], a[2], mx * D2R, my * D2R, mz * D2R, dt);
+    madgwick.update(gx * D2R, gy * D2R, gz * D2R, G[0], G[1], G[2], mx * D2R, my * D2R, mz * D2R, dt);
 
     vec3.scaleAndAdd(V_velocity, V_velocity, cartesianToArray(acceleration), dt);
     vec3.scale(V_velocity, V_velocity, 0.99);
 
     const { w, x, y, z } = madgwick.getQuaternion();
     quat.set(Q_angle, x, y, z, w);
-  };
-
-  const end = () => {
-    vec3.copy(P_0_prev, P_0_curr);
-    vec3.copy(P_1_prev, P_1_curr);
-    vec3.copy(P_2_prev, P_2_curr);
   };
 
   const updateHeadMovement = (dt: number) => {
