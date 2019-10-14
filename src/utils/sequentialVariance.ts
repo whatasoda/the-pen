cec2152916f7d58e7f279ed6709f2a3e51510c2e
whatasoda <git@whatasoda.me>
2019-10-14 17:29:06 +0900

    feat: sequential variance

---
+++ src/utils/sequentialVariance.ts
@@ -0,0 +1,16 @@
+import sequential from './sequential';
+import { zeroPeak } from './converter';
+
+const tmp = new Float32Array(1);
+const sequentialVariance = (size: number, coef: number) => {
+  const seq = sequential('scalar', size);
+  return (input: number) => {
+    tmp[0] = input;
+    seq.push(tmp);
+    const ave = seq.reduce((acc, curr) => acc + curr[0], 0) / size;
+    const raw = seq.reduce((acc, curr) => acc + (curr[0] - ave) ** 2, 0) / size;
+    return zeroPeak(raw * coef);
+  };
+};
+
+export default sequentialVariance;
