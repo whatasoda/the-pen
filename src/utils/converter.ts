5d7f4a75a02f4f785a1244a01e6ab0102dc194fb
whatasoda <git@whatasoda.me>
2019-10-26 18:59:40 +0900

    feat: sqrSubtract

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,3 +1,4 @@
+const { SQRT2, abs, max } = Math;
 /**
  * The euler-order should be z-x-y.
  * See also: https://developers.google.com/web/fundamentals/native-hardware/device-orientation/?hl=en
@@ -9,6 +10,10 @@ export const eulerToArray = ({ alpha, beta, gamma }: EulerRotation): V3 => [alph
  */
 export const cartesianToArray = ({ x, y, z }: CartesianCoord): V3 => [z || 0, x || 0, y || 0];
 
-const { SQRT2, abs } = Math;
 // https://www.google.com/search?q=1-(sqrt(2)**-abs(x)-1)**2
 export const zeroPeak = (value: number) => 1 - (SQRT2 ** -abs(value) - 1) ** 2;
+
+export const sqrSubtract = (a: number, b: number) => a ** 2 - b ** 2;
+export const sqrPositiveSubtract = (a: number, b: number, threshold: number = 0) => {
+  return max(sqrSubtract(a, b) + threshold, 0);
+};
