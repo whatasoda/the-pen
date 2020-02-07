0fe9fd12273139a8c9f366512c37ad741a5b4a76
whatasoda <git@whatasoda.me>
2020-02-07 21:15:53 +0900

    chore: remove old threejs implementation

--- src/canvas/index.ts
+++
@@ -1,5 +0,0 @@
-import createSceneComponent from './Scene';
-import createThreeCanvas from './ThreeCanvas';
-import { useCanvasInternal, useCamera, useScene } from './shared';
-
-export { createSceneComponent, createThreeCanvas, useCanvasInternal, useCamera, useScene };
